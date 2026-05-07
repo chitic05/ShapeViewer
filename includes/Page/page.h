dadfe3970c0de377d38a4becb114f3acb5aa91b1
chitic05 <chiticdavid05@gmail.com>
2026-05-07 11:16:46 +0300

    Final2

--- includes/Page/page.h
+++ includes/Page/page.h
@@ -4,14 +4,18 @@
 #include <string>
 #include <vector>
 
+// Clasă abstractă - nu poate fi instantiată direct
+// Conține cel puțin 2 funcții pur virtuale
 class Page
 {
 
 public:
     Page();
 
+    // Funcție pur virtuală - trebuie suprascrisă în clasele derivate
     virtual void initNeighbourPages() = 0;
 
+    // Funcție pur virtuală - trebuie suprascrisă în clasele derivate
     virtual void Load() = 0;
 
     std::string getName();
